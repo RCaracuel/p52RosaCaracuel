045d68cebeb035ffd673b09baa4c89b600825281
RCaracuel <rosa.caracuel@gmail.com>
2020-01-24 14:20:22 +0100

    Creación de constructores y métodos

--- src/main/java/daw/rcaracuelweb/Cliente.java
+++ src/main/java/daw/rcaracuelweb/Cliente.java
@@ -27,6 +27,36 @@ public class Cliente {
         this.email="email@email.com";
         this.edad=24;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", email=" + email + ", edad=" + edad + '}';
+    }
+    
     
     
     
